a9631f79153d847646bef1e2cb3913c611e88343
Tiago Villa <tiago_villa@hotmail.com>
2022-09-17 23:14:06 -0300

    Refactor TCP classes

--- tcp_client.js
+++ tcp_client.js
@@ -1,72 +1,45 @@
 //---------------------client----------------------
-var net = require('net')
+const net = require('net')
 const fs = require('fs')
 const crypto = require('crypto')
 
-// creating a custom socket client and connecting it....
-var client = new net.Socket()
+// cria socket do client
+let client = new net.Socket()
 client.connect({
   port: 8080,
 })
 
 client.on('connect', async function () {
-  console.log('Client: connection established with server')
+  console.log('Conexão estabelecida com o servidor')
 
-  console.log('---------client details -----------------')
-  var address = client.address()
-  var port = address.port
-  var family = address.family
-  var ipaddr = address.address
-  console.log('Client is listening at port' + port)
-  console.log('Client ip :' + ipaddr)
-  console.log('Client is IP4/IP6 : ' + family)
+  console.log('---------detalhes do client -----------------')
+  let address = client.address()
+  let port = address.port
+  let family = address.family
+  let ipaddr = address.address
+  console.log('Client utilizando a porta: ' + port)
+  console.log('IP do client: ' + ipaddr)
+  console.log('Client é IP4/IP6: ' + family)
 
-  // const filePath = path.join(folderpath, filename);
-
-  // let fileContent = await fs.readFile(filePath);
-  // writing data to server
+  // escreve dados no servidor
   const fileTeste = await fs.readFileSync('./testegrande.txt')
-  // console.log(fileTeste);
+  // const fileTeste = await fs.readFileSync('./teste.txt')
+
   const message = new Buffer.from(fileTeste)
 
   // Gera hash do buffer enviado
   let hash = crypto
     .createHash('md5')
-    .update(Uint8Array.from(fileTeste))
+    .update(Uint8Array.from(message))
     .digest('hex')
   console.log(`Hash do arquivo enviado: ${hash}`)
 
   client.write(message)
-  // client.write('hello from client')
 })
 
 client.setEncoding('utf8')
 
 client.on('data', function (data) {
-  console.log('Data from server:' + data)
+  let bread = client.bytesRead
+  console.log(`Bytes lidos: ${bread}`)
 })
-
-// setTimeout(function () {
-//   client.end('Bye bye server')
-// }, 50000)
-
-//NOTE:--> all the events of the socket are applicable here..in client...
-
-// -----------------creating client using net.connect instead of custom socket-------
-
-// server creation using net.connect --->
-// u can also => write the below code in seperate js file
-// open new node instance => and run it...
-
-// const clients = net.connect({ port: 8080 }, () => {
-//   // 'connect' listener
-//   console.log('connected to server!')
-//   clients.write('world!\r\n')
-// })
-// clients.on('data', (data) => {
-//   console.log(data.toString())
-//   clients.end()
-// })
-// clients.on('end', () => {
-//   console.log('disconnected from server')
-// })
--- tcp_server.js
+++ tcp_server.js
@@ -1,68 +1,60 @@
-var net = require('net')
-var fs = require('fs')
+const net = require('net')
+const fs = require('fs')
 const crypto = require('crypto')
 
-// creates the server
-var server = net.createServer()
+// cria instância do servidor
+let server = net.createServer()
 
-//emitted when server closes ...not emitted until all connections closes.
+// ao encerrar o servidor
 server.on('close', function () {
-  console.log('Server closed !')
+  console.log('Servidor encerrado!')
 })
 
-// emitted when new client connects
+// ao conectar com um novo client
 server.on('connection', function (socket) {
-  //this property shows the number of characters currently buffered to be written. (Number of characters is approximately equal to the number of bytes to be written, but the buffer may contain strings, and the strings are lazily encoded, so the exact number of bytes is not known.)
-  //Users who experience large or growing bufferSize should attempt to "throttle" the data flows in their program with pause() and resume().
+  console.log('Tamanho do buffer: ' + socket.bufferSize)
 
-  console.log('Buffer size : ' + socket.bufferSize)
+  console.log('---------detalhes do servidor -----------------')
 
-  console.log('---------server details -----------------')
+  let address = server.address()
+  let port = address.port
+  let family = address.family
+  let ipaddr = address.address
+  console.log('Servidor utilizando a porta: ' + port)
+  console.log('IP do servidor :' + ipaddr)
+  console.log('Servidor é IP4/IP6 : ' + family)
 
-  var address = server.address()
-  var port = address.port
-  var family = address.family
-  var ipaddr = address.address
-  console.log('Server is listening at port' + port)
-  console.log('Server ip :' + ipaddr)
-  console.log('Server is IP4/IP6 : ' + family)
+  let lport = socket.localPort
+  let laddr = socket.localAddress
+  console.log('Servidor utilizando a porta local: ' + lport)
+  console.log('IP local do servidor :' + laddr)
 
-  var lport = socket.localPort
-  var laddr = socket.localAddress
-  console.log('Server is listening at LOCAL port' + lport)
-  console.log('Server LOCAL ip :' + laddr)
+  console.log('------------detalhes do cliente remoto --------------')
 
-  console.log('------------remote client info --------------')
+  let rport = socket.remotePort
+  let raddr = socket.remoteAddress
+  let rfamily = socket.remoteFamily
 
-  var rport = socket.remotePort
-  var raddr = socket.remoteAddress
-  var rfamily = socket.remoteFamily
-
-  console.log('REMOTE Socket is listening at port' + rport)
-  console.log('REMOTE Socket ip :' + raddr)
-  console.log('REMOTE Socket is IP4/IP6 : ' + rfamily)
+  console.log('Socket remoto utilizando a porta: ' + rport)
+  console.log('IP do socket remoto: ' + raddr)
+  console.log('Socket remoto é IP4/IP6: ' + rfamily)
 
   console.log('--------------------------------------------')
-  //var no_of_connections =  server.getConnections(); // sychronous version
   server.getConnections(function (error, count) {
-    console.log('Number of concurrent connections to the server : ' + count)
+    console.log('Número de conexões simultâneas no servidor: ' + count)
   })
 
   socket.setEncoding('utf8')
 
   socket.setTimeout(800000, function () {
-    // called after timeout -> same as socket.on('timeout')
-    // it just tells that soket timed out => its ur job to end or destroy the socket.
-    // socket.end() vs socket.destroy() => end allows us to send final data and allows some i/o activity to finish before destroying the socket
-    // whereas destroy kills the socket immediately irrespective of whether any i/o operation is goin on or not...force destry takes place
-    console.log('Socket timed out')
+    console.log('Socket timeout')
   })
 
   socket.on('data', function (data) {
-    var bread = socket.bytesRead
-    var bwrite = socket.bytesWritten
-    console.log('Bytes read : ' + bread)
-    console.log('Bytes written : ' + bwrite)
+    let bread = socket.bytesRead
+    let bwrite = socket.bytesWritten
+    console.log('Bytes lidos: ' + bread)
+    console.log('Bytes escritos: ' + bwrite)
 
     // Gera hash do buffer recebido
     let hash = crypto
@@ -73,90 +65,66 @@ server.on('connection', function (socket) {
     console.log(`Hash do arquivo recebido: ${hash}`)
 
     fs.writeFileSync('./serverReceiveData.txt', data, 'utf-8')
-    console.log('Data sent to server : ' + data)
 
-    //echo data
-    var is_kernel_buffer_full = socket.write('Data ::' + data)
-    if (is_kernel_buffer_full) {
-      console.log(
-        'Data was flushed successfully from kernel buffer i.e written successfully!'
-      )
+    let isBufferFull = socket.write('Data ::' + data)
+    if (isBufferFull) {
+      console.log('Flush de dados do buffer finalizado com sucesso')
     } else {
       socket.pause()
     }
   })
 
+  // esvazia buffer
   socket.on('drain', function () {
-    console.log('write buffer is empty now .. u can resume the writable stream')
+    console.log('buffer de dados vazio')
     socket.resume()
   })
 
   socket.on('error', function (error) {
-    console.log('Error : ' + error)
+    console.log('Erro : ' + error)
   })
 
   socket.on('timeout', function () {
-    console.log('Socket timed out !')
+    console.log('Socket timeout')
     socket.end('Timed out!')
-    // can call socket.destroy() here too.
   })
 
   socket.on('end', function (data) {
-    console.log('Socket ended from other end!')
-    console.log('End data : ' + data)
+    console.log('Socket encerrado')
+    console.log('Dados : ' + data)
   })
 
   socket.on('close', function (error) {
-    var bread = socket.bytesRead
-    var bwrite = socket.bytesWritten
-    console.log('Bytes read : ' + bread)
-    console.log('Bytes written : ' + bwrite)
-    console.log('Socket closed!')
+    let bread = socket.bytesRead
+    let bwrite = socket.bytesWritten
+    console.log('Bytes lidos: ' + bread)
+    console.log('Bytes escritos: ' + bwrite)
+    console.log('Socket finalizado')
     if (error) {
-      console.log('Socket was closed coz of transmission error')
+      console.log('Socket foi finalizado por erro de transmissão')
     }
   })
 
   setTimeout(function () {
-    var isdestroyed = socket.destroyed
-    console.log('Socket destroyed:' + isdestroyed)
+    let isdestroyed = socket.destroyed
+    console.log('Socket finalizado:' + isdestroyed)
     socket.destroy()
   }, 1200000)
 })
 
-// emits when any error occurs -> calls closed event immediately after this.
+// quando há erros de transmissão
 server.on('error', function (error) {
-  console.log('Error: ' + error)
+  console.log('Erro: ' + error)
 })
 
-//emits when server is bound with server.listen
+// pronto para receber conexões
 server.on('listening', function () {
-  console.log('Server is listening!')
+  console.log('Servidor aguardando conexões')
 })
 
 server.maxConnections = 10
 
-//static port allocation
 server.listen(8080)
-
-// for dyanmic port allocation
-// server.listen(function () {
-//   var address = server.address()
-//   var port = address.port
-//   var family = address.family
-//   var ipaddr = address.address
-//   console.log('Server is listening at port' + port)
-//   console.log('Server ip :' + ipaddr)
-//   console.log('Server is IP4/IP6 : ' + family)
-// })
-
-var islistening = server.listening
-
-if (islistening) {
-  console.log('Server is listening')
-} else {
-  console.log('Server is not listening')
-}
 
 setTimeout(function () {
   server.close()
